c21b1e4d53e4df111d2c6992ffe35e4797e05928
Alex <user@nomail.com>
2018-06-08 19:31:42 +0700

    buttons with different states via params, no ripple

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,11 +1,2 @@
-import 'ripple-jquery';
 
-const options = {
-	bgColor: 'white', //default is rgba(0,0,0,0.2),
-	time: 400
-}
 
-$(document).ready(function() {
-		$('.button').ripple(options);
-		console.log($('.button'))
-});
