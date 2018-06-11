c46821d08e85fc13b36dd8226dfc99be181f35b0
Alex <user@nomail.com>
2018-06-11 23:02:09 +0700

    sliders

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import $ from 'jquery'
 import './components/button/button.js'
 import './components/circlecharts'
+import './components/slider'
 console.log($)
 $(document).ready(()=>{
   let str = `window location is ${window.location}`;
---
+++ src/components/slider/index.js
@@ -0,0 +1,38 @@
+import $ from "jquery"
+import noUiSlider from "nouislider"
+
+console.log(noUiSlider)
+
+var sliders = $('.slider');
+
+sliders.each((i, el) => { 
+	const maxValue = $(el).data('max-value');
+	const parts = $(el).data('parts');
+
+	noUiSlider.create(el, {
+	    range: {
+	        min: 0,
+	        max: maxValue
+	    },
+	    tooltips: [ true ],
+	    start: [ Math.floor(maxValue / 2) ],
+	    step: parts ? Math.floor(maxValue / (parts - 1)) : undefined,
+	    pips: parts > 2 ? { mode: 'count', values: parts  } : undefined
+	})
+
+	if (parts) {
+		var pips = el.querySelectorAll('.noUi-value');
+		function clickOnPip ( ) {
+		    var value = Number(this.getAttribute('data-value'));
+		    el.noUiSlider.set(value);
+		}
+
+		for ( var i = 0; i < pips.length; i++ ) {
+		    // For this example. Do this in CSS!
+		    pips[i].style.cursor = 'pointer';
+		    pips[i].addEventListener('click', clickOnPip);
+		}
+	}
+	
+	}
+)
