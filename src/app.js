5b491c5197a97659b8556d6b8ead08f98f24dee7
Alex <user@nomail.com>
2018-06-13 17:15:25 +0700

    stages

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import $ from 'jquery'
 import './components/button/button.js'
 import './components/circlecharts'
 import './components/slider'
+import './components/stage'
 console.log($)
 $(document).ready(()=>{
   let str = `window location is ${window.location}`;
