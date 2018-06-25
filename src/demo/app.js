158f562ef8a573ac5a15f9e236682f329d1f4f9d
Alex <16453186+alextsk@users.noreply.github.com>
2018-06-25 16:55:10 +0700

    two sliders installed on photo page

--- src/demo/app.js
+++ src/demo/app.js
@@ -1,2 +1,19 @@
 import $ from 'jquery'
+import Glide from '@glidejs/glide'
+import slick from 'slick-carousel'
 import '../components/video'
+
+ new Glide('.glide', {
+  type: 'carousel',
+  startAt: 0,
+  perView: 5
+}).mount()
+
+ $('.js-carousel__list').slick({
+  infinite: true,
+  slidesToShow: 5,
+  slidesToScroll: 5,
+  centerMode: true,
+  prevArrow:$(".carousel__controls .arrow-button--left"),
+  nextArrow:$(".carousel__controls .arrow-button--right")
+});
