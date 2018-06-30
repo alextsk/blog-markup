4c9f34359293c8f1b6dcc83f7c31f5c53cbcc9fa
Alex <16453186+alextsk@users.noreply.github.com>
2018-06-30 23:17:56 +0700

    handmade grid and carousel on photo page

--- src/demo/app.js
+++ src/demo/app.js
@@ -3,17 +3,47 @@ import Glide from '@glidejs/glide'
 import slick from 'slick-carousel'
 import '../components/components.js'
 
- new Glide('.glide', {
+if ($(".glide").length) {
+  new Glide('.glide', {
   type: 'carousel',
   startAt: 0,
   perView: 5
-}).mount()
+  }).mount()
+}
 
- $('.js-carousel__list').slick({
-  infinite: true,
-  slidesToShow: 5,
-  slidesToScroll: 5,
-  centerMode: true,
-  prevArrow:$(".carousel__controls .arrow-button--left"),
-  nextArrow:$(".carousel__controls .arrow-button--right")
-});
+if ($('.js-carousel__list').length) {
+  $('.js-carousel__list').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    centerMode: true,
+    prevArrow: $(".carousel__controls .arrow-button--left"),
+    nextArrow: $(".carousel__controls .arrow-button--right")
+  });
+}
+
+const img = $(".js-big-image > img")
+var prevTarget = null
+var target = null
+
+$(".photos__item")
+.click( evt => {
+  activate($(evt.currentTarget), prevTarget)
+})
+
+activate($($(".photos__item")[0]))
+
+function activate(target) {
+  img.attr('src', target.data("image"))
+  if (prevTarget) prevTarget.removeClass("active") 
+  target.addClass("active")
+  prevTarget = target
+}
+
+$(".arrow-button--left")
+.click(() => {
+  activate(prevTarget.prev().length ? prevTarget.prev() : $(".photos__item").last())
+})
+
+$(".arrow-button--right")
+.click(() => activate(prevTarget.next().length ? prevTarget.next() : $(".photos__item").first()))
