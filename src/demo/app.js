6305d5bbc2536f25f5608177b2c7bf7343033c7e
Alex <16453186+alextsk@users.noreply.github.com>
2018-07-02 21:48:10 +0700

    index page adaptive

--- src/demo/app.js
+++ src/demo/app.js
@@ -1,26 +1,5 @@
 import $ from 'jquery'
-import Glide from '@glidejs/glide'
-import slick from 'slick-carousel'
 import '../components/components.js'
-
-if ($(".glide").length) {
-  new Glide('.glide', {
-  type: 'carousel',
-  startAt: 0,
-  perView: 5
-  }).mount()
-}
-
-if ($('.js-carousel__list').length) {
-  $('.js-carousel__list').slick({
-    infinite: true,
-    slidesToShow: 5,
-    slidesToScroll: 5,
-    centerMode: true,
-    prevArrow: $(".carousel__controls .arrow-button--left"),
-    nextArrow: $(".carousel__controls .arrow-button--right")
-  });
-}
 
 const img = $(".js-big-image > img")
 var prevTarget = null
