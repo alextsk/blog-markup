4e44c7361c79fc92c9600fbc2790171af0d5fa71
Alex <16453186+alextsk@users.noreply.github.com>
2018-08-09 17:39:41 +0700

    news section to template

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -7,6 +7,7 @@ $(function() {
   let currentDate = null
   const head = $(".calendar__head")
   const footer = $(".calendar__footer")
+  const today = $(".js-calendar-today")
   var calendar = $('.datepicker').datepicker({
     prevText: '<svg width=16 height=25 transform="rotate(180.1)">' + $(".arrow-icon").html() + "</svg>",
     nextText: '<svg width=16 height=25>' + $(".arrow-icon").html() + "</svg>",
@@ -20,5 +21,7 @@ $(function() {
   calendar.prepend(head)
   calendar.append(footer)
   currentDate = calendar.datepicker( "getDate" );
+  today.click(() => console.log(calendar.datepicker( "setDate", "10/12/2018" )))
+
 });
 
