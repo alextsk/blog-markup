914928300038fc6f3337cd8a5d89caf7427cfd5e
Alex <16453186+alextsk@users.noreply.github.com>
2018-08-26 22:26:38 +0700

    piecharts refactored as a class

--- src/components/circlecharts/index.js
+++ src/components/circlecharts/index.js
@@ -1,31 +1,36 @@
 import $ from "jquery";
 import createSVGDoughnut from './vendor/create-svg-doughnut/src/createSVGDoughnut';
 
-function createSvgNode(n, v) {
-  let node = document.createElementNS("http://www.w3.org/2000/svg", n);
-  Object.keys(v).forEach((p) => node.setAttributeNS(null, p, v[p]))
-  return node
+
+class Chart {
+  constructor(element) {
+    const segments = $(element).data('chart-segments');
+    const innerRadius = +$(element).data('chart-inner-radius');
+    const outerRadius = +$(element).data('chart-outer-radius');
+    const chartText = $(element).data('chart-text') !== undefined ? $(element).data('chart-text').toString() : '';
+    const vals = segments.map( seg => +seg[0] )
+    const pals = segments.map( seg => seg[1] )
+    const doughnut = createSVGDoughnut(vals, outerRadius, innerRadius, pals);
+    const svgText = this.createSvgNode('text', { x: 47, y: 60, class: "text-in-circle", 'text-anchor':"middle" });
+    const textNode = document.createTextNode(chartText);
+    
+    svgText.appendChild(textNode);
+    doughnut.appendChild(svgText);
+    element.appendChild(doughnut)
+  }
+
+  createSvgNode(nodeName, nodeAttributes) {
+    let node = document.createElementNS("http://www.w3.org/2000/svg", nodeName);
+    Object.keys(nodeAttributes).forEach(attribute => node.setAttributeNS(null, attribute, nodeAttributes[attribute]));
+    return node;
+  }
 }
 
-function createChart(el) {
-  const segments = $(el).data('chart-segments');
-  const innerRadius = +$(el).data('chart-inner-radius');
-  const outerRadius = +$(el).data('chart-outer-radius');
-  const chartText = $(el).data('chart-text') !== undefined ? $(el).data('chart-text').toString() : '';
-  const vals = segments.map( seg => +seg[0] )
-  const pals = segments.map( seg => seg[1] )
-  const doughnut = createSVGDoughnut(vals, outerRadius, innerRadius, pals);
-  const svgText = createSvgNode('text', { x: 47, y: 60, class: "text-in-circle", 'text-anchor':"middle" });
-  const textNode = document.createTextNode(chartText);
-  
-  svgText.appendChild(textNode);
-  doughnut.appendChild(svgText);
-  el.appendChild(doughnut)
-}
+
 
 $(document).ready(() => {
-  $(".percentage").each((i, element) => createChart(element));
-  $(".donut").each((i, element) => createChart(element));
+  $(".percentage").each((i, element) => new Chart(element));
+  $(".donut").each((i, element) => new Chart(element));
 })
 
 
