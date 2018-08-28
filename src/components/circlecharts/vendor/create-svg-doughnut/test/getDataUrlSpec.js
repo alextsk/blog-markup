9d51a4ba1ea367e621a5fba475f9c337d629d4d8
Alex <16453186+alextsk@users.noreply.github.com>
2018-08-28 19:33:51 +0700

    made an explicit dependency from git rep for piecharts

--- src/components/circlecharts/vendor/create-svg-doughnut/test/getDataUrlSpec.js
+++
@@ -1,9 +0,0 @@
-/*global describe it*/
-import assert from 'assert';
-import createSVGPie from '../src/createSVGPie.js';
-
-describe('createSVGPie()', function () {
-    it('finds that true is true.', function () {
-        assert.equal(true, true);
-    });
-});
